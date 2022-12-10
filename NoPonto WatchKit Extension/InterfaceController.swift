7064fb73d6105cf9f2e8343ce8b4d79d0761b92c
Danilo Santos <danilopsnts@gmail.com>
2022-12-10 15:26:01 -0300

    add hidden text and image

--- NoPonto WatchKit Extension/InterfaceController.swift
+++ NoPonto WatchKit Extension/InterfaceController.swift
@@ -11,8 +11,21 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    @IBOutlet weak var buttonTimer: WKInterfaceButton!
+    @IBOutlet weak var labelWeight: WKInterfaceLabel!
+    @IBOutlet weak var groupText: WKInterfaceGroup!
+    @IBOutlet weak var groupImage: WKInterfaceGroup!
+    @IBOutlet weak var labelCook: WKInterfaceLabel!
+    @IBOutlet weak var sliderCook: WKInterfaceSlider!
+    @IBOutlet weak var pickerCook: WKInterfacePicker!
+    @IBOutlet weak var pickerWeight: WKInterfacePicker!
+    @IBOutlet weak var labelCook2: WKInterfaceLabel!
+    
+    
+    
     override func awake(withContext context: Any?) {
-        // Configure interface objects here.
+        groupImage.setHidden(true)
     }
     
     override func willActivate() {
@@ -23,4 +36,25 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
     }
 
+    @IBAction func toggleTimer() {
+    }
+    
+    @IBAction func minus() {
+    }
+    
+    @IBAction func plus() {
+    }
+    
+    @IBAction func onSliderChange(_ value: Float) {
+    }
+    
+    @IBAction func onWeightPickerChange(_ value: Int) {
+    }
+    @IBAction func onCookPickerChange(_ value: Int) {
+    }
+    
+    @IBAction func onModeChange(_ value: Bool) {
+        groupText.setHidden(value)
+        groupImage.setHidden(!value)
+    }
 }
